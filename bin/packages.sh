5742d90cf4b4ffc3347c8509657f52abab156276
Phil Thompson <phil@electricvisions.com>
2014-11-17 22:07:12 +0000

    fixes, cleanup, missing packages

--- bin/packages.sh
+++ bin/packages.sh
@@ -1,14 +1,17 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
 aur 'https://aur.archlinux.org/packages/rb/rbenv/rbenv.tar.gz'
 aur 'https://aur.archlinux.org/packages/ru/ruby-build/ruby-build.tar.gz'
-rbenv install 2.1.5
+rbenv install 2.1.4
+rbenv global 2.1.4
 gem install mdl ruby-lint
 
 pac nodejs unison
-npm install -g jscs jshint jsonlint
+sudo npm install -g jscs jshint jsonlint
 
-if [ -z `pacman -Q xorg-server` ]; then
+pacman -Q xorg-server
+if [ $? -eq 0 ]; then
+  pac alsa-lib flac gconf harfbuzz-icu icu libxtst nss opus snappy speech-dispatcher xdg-utils
   aur 'https://aur.archlinux.org/packages/go/google-chrome/google-chrome.tar.gz'
   aur 'https://aur.archlinux.org/packages/ur/urxvt-clipboard/urxvt-clipboard.tar.gz'
 fi
