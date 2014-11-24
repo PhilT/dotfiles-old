9109cef10a7465ffd2f7b1f92921096627717433
Phil Thompson <phil@electricvisions.com>
2014-11-24 01:14:50 +0000

    support version changes in AUR builds

--- bin/packages.sh
+++ bin/packages.sh
@@ -10,7 +10,7 @@ RUBY_VERSION=2.1.4
 
 aur 'rb' 'rbenv'
 aur 'ru' 'ruby-build'
-aur 'im' 'imagemagick-no-hdri'
+aur 'im' 'imagemagick-no-hdri' '6.9.0.0' '6bf4263ceaeea61e00fe15a95db320d49bcc48c4'
 rbenv version | grep $RUBY_VERSION || rbenv install $RUBY_VERSION
 rbenv global $RUBY_VERSION
 rbenv rehash
