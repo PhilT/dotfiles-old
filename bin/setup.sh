4102e66674519328d28c9e85087fd24bab5c5c0d
Phil Thompson <phil@electricvisions.com>
2014-11-25 21:46:18 +0000

    typo

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,10 +4,10 @@ export DESKTOP=evm
 export LAPTOP=evx
 export SERVER=evs
 
-ssh-keyscan -H bitbucket.org | >> ~/.ssh/known_hosts
 dir=`dirname $0`
 
 $dir/network.sh
+ssh-keyscan bitbucket.org -t rsa >> ~/.ssh/known_hosts
 $dir/bin_setup.sh
 $dir/vim_plugins.sh
 $dir/repos.sh
