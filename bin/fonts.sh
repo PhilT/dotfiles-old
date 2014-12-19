5560e390c98ec387b1e7348f3a65c0b3eb3ea66c
Phil Thompson <phil@electricvisions.com>
2014-12-19 09:53:51 +0000

    temp fix for dirmngr bug

--- bin/fonts.sh
+++ bin/fonts.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ `pacman -Q xorg-server 2>/dev/null` != '' ]]; then
+
+  sudo -i dirmngr < /dev/null # fixes https://bugs.archlinux.org/task/42798
+
   echo -e '[infinality-bundle]\nServer = http://bohoomil.com/repo/$arch' | sudo tee -a /etc/pacman.conf >> /dev/null
   echo -e '[infinality-bundle-multilib]\nServer = http://bohoomil.com/repo/multilib/$arch' | sudo tee -a /etc/pacman.conf >> /dev/null
   echo -e '[infinality-bundle-fonts]\nServer = http://bohoomil.com/repo/fonts'  | sudo tee -a /etc/pacman.conf >> /dev/null
@@ -11,3 +14,4 @@ if [[ `pacman -Q xorg-server 2>/dev/null` != '' ]]; then
   sudo pacman -Rdd --noconfirm --noprogressbar ttf-dejavu
   pac ibfonts-meta-base
 fi
+
