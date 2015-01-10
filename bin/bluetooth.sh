8a03db43745a78368824ce8a2a016e1ce8ae293d
Phil Thompson <phil@electricvisions.com>
2015-01-10 18:19:29 +0000

    ensure bluetooth enabled on boot

--- bin/bluetooth.sh
+++ bin/bluetooth.sh
@@ -6,3 +6,5 @@ pac bluez bluez-utils expect
 sudo systemctl enable bluetooth
 sudo systemctl restart bluetooth
 
+echo ACTION=="add", KERNEL=="hci0", RUN+="/usr/bin/hciconfig hci0 up" | tee -a sudo /etc/udev/rules.d/10-local.rules >> /dev/null
+
