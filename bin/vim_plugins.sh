c0d2b51883fd6381b7c0f1924fb029cbe9c65b94
Phil Thompson <phil@electricvisions.com>
2016-02-17 07:39:52 +0000

    removed unused vim plugins, added JS ones that work, update syntax in conkyrc and add basic config for Syntastic

--- bin/vim_plugins.sh
+++ bin/vim_plugins.sh
@@ -9,7 +9,6 @@ fzf/install
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 
-gup ack.vim mileszs --depth 1
 gup vim-airline bling --depth 1
 gup vim-bundler tpope --depth 1
 gup vim-dispatch tpope --depth 1
@@ -17,25 +16,22 @@ gup vim-fugitive tpope --depth 1
 gup fzf.vim junegunn --depth 1
 gup vim-git tpope --depth 1
 gup vim-gitgutter airblade --depth 1
-gup vim-haml tpope --depth 1
-gup jasmine.vim claco --depth 1
-cd ~/.vim/bundle/JavaScript-Indent > /dev/null 2>&1 && git checkout .
-cd - > /dev/null
-gup JavaScript-Indent vim-scripts --depth 1
-sed -i '/echo "Sourcing/d' ~/.vim/bundle/JavaScript-Indent/indent/html.vim
-gup vim-javascript-syntax jelera --depth 1
+gup vim-js-indent gavocanov --depth 1
 gup vim-markdown tpope --depth 1
 gup nerdtree scrooloose --depth 1
 gup vim-node moll --depth 1
 gup vim-pathogen tpope --depth 1
 gup vim-rails tpope --depth 1
 gup vim-rake tpope --depth 1
-gup vim-test janko-m --depth 1
 gup vim-ruby vim-ruby --depth 1
 gup vim-slim slim-template --depth 1
+gup vim-slm slm-lang --depth 1
+gup vim-stylus wavded --depth 1
 gup snipmate.vim msanders --depth 1
 gup syntastic scrooloose --depth 1
 gup vim-surround tpope --depth 1
+gup vim-test janko-m --depth 1
+gup yajs.vim othree --depth 1
 gup vim-yaml avakhov --depth 1
 gup ag.vim rking --depth 1
 
@@ -44,4 +40,3 @@ mkdir -p ~/.vim/colors
 cd ~/.vim/colors
 curl -s -O https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 curl -s -O https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim
-
